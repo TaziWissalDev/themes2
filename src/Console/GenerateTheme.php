5bdc648a14e5041a34886f6d4cd9736f6f78e310
Heisenberg00 <173586@supinfo.com>
2021-02-12 10:18:15 +0100

    Update GenerateTheme.php
    extend resources from other theme

--- src/Console/GenerateTheme.php
+++ src/Console/GenerateTheme.php
@@ -13,7 +13,7 @@ class GenerateTheme extends Command
      *
      * @var string
      */
-    protected $signature = 'make:theme {slug} {--quick}';
+    protected $signature = 'make:theme {slug} {from} {--quick}';
 
     /**
      * The console command description.
@@ -39,32 +39,38 @@ class GenerateTheme extends Command
      */
     public function handle()
     {
+
         $options     = $this->getOptions();
+
         $root        = base_path('themes');
         $stubsPath   = __DIR__ . '/../../resources/stubs/theme';
         $slug        = $options['slug'];
+        $from        = $options['from'];
         $name        = $this->format($slug);
+        // $var = $this->format($from);
 
         if (File::isDirectory($root . '/' . $name)) {
             return $this->error('Theme already exists!');
         }
 
-        if (! File::isDirectory($root)) {
+        if (!File::isDirectory($root)) {
             File::makeDirectory($root);
         }
 
         foreach (File::allFiles($stubsPath) as $file) {
             $contents = $this->replacePlaceholders($file->getContents(), $options);
             $subPath  = $file->getRelativePathname();
-            $filePath = $root.'/'.$options['name'].'/'.$subPath;
+            $filePath = $root . '/' . $options['name'] . '/' . $subPath;
             $dir      = dirname($filePath);
 
-            if (! File::isDirectory($dir)) {
+            if (!File::isDirectory($dir)) {
                 File::makeDirectory($dir, 0755, true);
             }
 
-            File::put($filePath, $contents);
+            File::put($filePath, $contents, $options);
         }
+
+        $this->cloneTheme($slug, $from, $options);
 
         $this->info("Theme created successfully.");
     }
@@ -75,16 +81,22 @@ class GenerateTheme extends Command
     protected function getOptions()
     {
         $slug   = Str::slug($this->argument('slug'));
+        $from   = Str::slug($this->argument('from'));
         $name   = $this->format($slug);
+        $from   = $this->format($from);
         $quick  = $this->option('quick');
         $vendor = config('themes.vendor');
         $author = config('themes.author');
 
         return [
             'slug'              => $slug,
+            'from'              => $from,
             'namespace'         => "Themes\\$name",
             'escaped_namespace' => "Themes\\\\$name",
             'name'              => $quick ? $name : $this->ask('What is your theme\'s name?', $name),
+            'app_name'          => $quick ? $name : $this->ask('What is your App name?', "Globale academy"),
+            'database_name'              => $quick ? $name : $this->ask('What is your database name ?', "globalesecurite2"),
+            'app_prefix'              => $quick ? $name : $this->ask('What is your app prefix  ?', "gs21"),
             'author'            => $quick ? $author : $this->ask('Who is the author of your theme?', $author),
             'version'           => $quick ? '1.0.0' : $this->ask('What is the version of your theme?', '1.0.0'),
             'description'       => $quick ? "$name theme." : $this->ask('Can you describe your theme?', "$name theme."),
@@ -135,5 +147,51 @@ class GenerateTheme extends Command
     private function format($name)
     {
         return ucfirst(Str::camel($name));
+    }
+    // function to copy folders from anothet theme
+    protected function cloneTheme($slug, $from, $options)
+    {
+        $ressource_path = base_path('themes/' . $slug . '/resources');
+        $public_path = base_path('themes/' . $slug . '/resources');
+        $storage_path = base_path('themes/' . $slug . '/resources');
+        $config_path = base_path('themes/' . $slug . '/resources');
+        $slug = Str::ucfirst($slug);
+
+        if (file_exists($ressource_path)) {
+            File::deleteDirectory($ressource_path);
+            File::copyDirectory(base_path('themes/' . $from . '/resources'), base_path('themes/' . $slug . '/resources'));
+        } else {
+            File::copyDirectory(base_path('themes/' . $from . '/resources'), base_path('themes/' . $slug . '/resources'));
+        }
+        if (file_exists($public_path)) {
+            File::deleteDirectory($public_path);
+            File::copyDirectory(base_path('themes/' . $from . '/public'), base_path('themes/' . $slug . '/public'));
+        } else {
+            File::copyDirectory(base_path('themes/' . $from . '/public'), base_path('themes/' . $slug . '/public'));
+        }
+        if (file_exists($storage_path)) {
+            File::deleteDirectory($storage_path);
+            File::copyDirectory(base_path('themes/' . $from . '/storage'), base_path('themes/' . $slug . '/storage'));
+        } else {
+            File::copyDirectory(base_path('themes/' . $from . '/storage'), base_path('themes/' . $slug . '/storage'));
+        }
+        if (file_exists($config_path)) {
+            File::deleteDirectory($config_path);
+            File::copyDirectory(base_path('themes/' . $from . '/Config'), base_path('themes/' . $slug . '/Config'));
+        } else {
+            File::copyDirectory(base_path('themes/' . $from . '/Config'), base_path('themes/' . $slug . '/Config'));
+        }
+
+        $json_path =  theme_path('theme.json', $slug);
+
+        // if (file_exists($json_path)) {
+        //     File::delete($json_path);
+        //     File::copyDirectory(theme_path('theme.json', $from), theme_path('theme.json', $slug));
+        // }
+        $theme_json = json_decode(File::get(theme_path('theme.json', $slug)));;
+        $theme_json->APP_NAME = $options['app_name'];
+        $theme_json->DB_DATABASE = $options['database_name'];
+        $theme_json->APP_PREFIX = $options['app_prefix'];
+        $theme_json = File::put(theme_path('theme.json', $slug), json_encode($theme_json));
     }
 }
